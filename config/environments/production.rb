6a4ff8f5042808ac47e3aa5fe834ba6006be8d70
Will Megson <wmegson@GM13146.local>
2012-12-08 18:06:55 -0600

    hooraa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 SampleApp::Application.configure do
 
-config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
@@ -17,7 +16,7 @@ config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
