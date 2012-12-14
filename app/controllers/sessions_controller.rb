aa4f835ed1db295a7db409fb38a05fdd24d75b5d
Will Megson <wmegson@GM13146.local>
2012-12-13 22:05:08 -0600

    working user signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,8 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-#       Handle successful login
+		sign_in user
+		redirect_to user
 	end
   end
   
